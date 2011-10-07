993e103be4804f2eb1027466b7aa2d26315726a8
Chas Emerick <cemerick@snowtide.com>
2011-10-07 12:11:32 -0400

    project setup

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject com.cemerick/clutch-clojurescript "0.3.0-SNAPSHOT"
+  :description "Experimental support for defining CouchDB views in ClojureScript via Clutch."
+  :url "http://github.com/cemerick/clutch-clojurescript"
+  :dependencies [[com.ashafa/clutch "0.2.5"]]
+  :dev-dependencies [[lein-clojars "0.7.0"]])
