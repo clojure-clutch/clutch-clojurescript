6fec2225ac34d1f6e773a1624a9bb6a3a781038d
Chas Emerick <cemerick@snowtide.com>
2011-10-10 07:52:12 -0400

    fix version number and clutch dependency

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject com.cemerick/clutch-clojurescript "0.3.0-SNAPSHOT"
+(defproject com.cemerick/clutch-clojurescript "0.0.1-SNAPSHOT"
   :description "Experimental support for defining CouchDB views in ClojureScript via Clutch."
   :url "http://github.com/cemerick/clutch-clojurescript"
-  :dependencies [[com.ashafa/clutch "0.2.5"]]
+  :dependencies [[com.ashafa/clutch "0.3.0-SNAPSHOT"]]
   :dev-dependencies [[lein-clojars "0.7.0"]])
